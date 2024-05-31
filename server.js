d44e3b79b35da082bd2beccf2252576340a4a7b9
Potism <felixbakattv@gmail.com>
2024-05-31 04:08:17 +0200

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const serverClient = new StreamChat(apiKey, apiSecret);
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend origin
+  origin: "https://chatappown.vercel.app", // Replace with your frontend origin
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
